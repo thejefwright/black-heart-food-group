5f0eea3cf8a1a6542bd52d209246f230a33e5486
Jef Wright <thejefwright@Jefs-MacBook-Pro-2.local>
2021-08-03 14:34:53 -0400

    adding second button

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,32 @@ import {Link} from 'gatsby';
 // import parse from "html-react-parser";
 
 const HomeStyles = styled.div`
-position: relative;
  display: grid;
  place-items: center;
  height: 100vh;
  text-align: center;
+ .wrapper {
+   display: flex;
+   flex-direction: column;
+   margin-top: 3rem ;
+ }
 .button {
-  position: absolute;
   color: white;
-  bottom: 10rem;
-  left: 50%;
-  transform: translateX(-50%);
   font-size: 2rem;
   border: 1px solid white;
   border-radius: 15px;
   padding: 1rem 3rem;
+  margin: 2rem;
   text-decoration: none;
+  transition: all 500ms;
   &:visited {
     color: white;
+  }
+  &:hover {
+    border: 1px solid var(--orange);
+    color: var(--blue);
+    background-color: var(--orange);
+
   }
 }
 
@@ -30,39 +38,20 @@ position: relative;
 
 
 export default function IndexPage() {
-//   const data = useStaticQuery(graphql`
-//    {
-//   allWpPost {
-//     nodes {
-//       id
-//       title
-//       excerpt
-//       uri
-//       content
-//     }
-//   }
-// }
-
-//   `)
-  // const { allWpPost }  = data;
   return (
     <HomeStyles>
-    <div>
-      <h1>Jef Wright</h1>
-      <h2>I'm working on it</h2>
-      <Link to="/menu" className="button">I'm Erica Archer</Link>
-        {/* {allWpPost.nodes.map((post) =>  (
-          <div key={post.id}>
-            <h1 dangerouslySetInnerHTML={{__html: post.title}} ></h1>
-            <h1>{parse(post.title)}</h1>
-            <p> {post.excerpt}</p>
-            
-            <hr />
-            <p >{parse(post.content)}</p>
-            </div>
-            ))} */}
+ 
+      <div>
+        <h1>Jef Wright</h1>
+        <h2>I'm working on it</h2>
+      <div className="wrapper">
+        <Link to="/vinaigrette" className="button">I'm Melissa</Link>
+        <Link to="/menu" className="button">I'm Erica</Link>
+      </div>
+      
+      </div>
           
-    </div>
+    
             </HomeStyles>
   )
 };
